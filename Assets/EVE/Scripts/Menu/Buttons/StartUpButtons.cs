c3293210bbb4414cbf3c23709ba5b638f6de4a3b
Jascha Grübel <jgruebel@student.ethz.ch>
2019-09-18 09:16:56 +0200

    Add startup menu and menu settings. Closes #120. Closes #122.

--- Assets/EVE/Scripts/Menu/Buttons/StartUpButtons.cs
+++ Assets/EVE/Scripts/Menu/Buttons/StartUpButtons.cs
@@ -6,16 +6,27 @@ namespace EVE.Scripts.Menu.Buttons
 {
     public class StartUpButtons : MonoBehaviour
     {
-        // Start is called before the first frame update
+        private LaunchManager _launchManager;
         private MenuManager _menuManager;
+        private Toggle _rememberToggle;
 
         void Start()
         {
-            _menuManager = GameObject.FindWithTag("LaunchManager").GetComponent<LaunchManager>().MenuManager;
+            _launchManager = GameObject.FindGameObjectWithTag("LaunchManager").GetComponent<LaunchManager>();
+            _menuManager = _launchManager.MenuManager;
 
-            var fields = transform.Find("Panel").Find("Fields").Find("Tiles");
-            fields.Find("Experiment Button").GetComponent<Button>().onClick.AddListener(() => { _menuManager.InstantiateAndShowMenu("Main Menu", "Launcher"); });
-            fields.Find("Evaluation Button").GetComponent<Button>().onClick.AddListener(() => { _menuManager.InstantiateAndShowMenu("Data Explorer Menu", "Launcher"); });
+            var fields = transform.Find("Panel").Find("Fields");
+            _rememberToggle = fields.Find("Remember Button").Find("Button").GetComponent<Toggle>();
+            fields.Find("Tiles").Find("Experiment Button").GetComponent<Button>().onClick.AddListener(() =>
+            {
+                _launchManager.ExperimentSettings.MenuSettings.FirstMenu = _rememberToggle.isOn ? "Main Menu" : "Startup Menu";
+                _menuManager.InstantiateAndShowMenu("Main Menu", "Launcher");
+            });
+            fields.Find("Tiles").Find("Evaluation Button").GetComponent<Button>().onClick.AddListener(() =>
+            {
+                _launchManager.ExperimentSettings.MenuSettings.FirstMenu = _rememberToggle.isOn ? "Data Explorer Menu" : "Startup Menu";
+                _menuManager.InstantiateAndShowMenu("Data Explorer Menu", "Launcher");
+            });
         }
     }
 }
